c41fa43f8fab5878ad3df806e22c0d3624402163
Adrien Thebo <adrien.thebo@gmail.com>
2011-04-24 23:42:34 -0700

    Added configuration processing for file lists

--- lib/copycfg/config.rb
+++ lib/copycfg/config.rb
@@ -14,22 +14,29 @@ module Copycfg::Config
     # Configuration from YAML
     attr_reader :config
 
+    # Loads configuration
     def loadconfig yamlfile
       File.open(yamlfile) { | yf | @config = YAML::load yf }
     end
 
+    # Creates a lists of files to copy for a host.
+    # This moves the burden of processing yaml from the host to the config
     def filelist host
       files = @config["hosts"][host]["files"]
 
       @config["hosts"][host]["filegroups"].each do | group |
         files += @config["filegroups"][group]
       end
+
+      if files.empty?
+        files += @config["filegroups"]["default"]
+      end
     end
 
+    # Gives direct access to the yaml structure
+    # In essence, Copycfg::Config["val"] vs Copycfg::Config.config["val"]
     def [](name)
       @config[name]
     end
   end
 end
-
-
