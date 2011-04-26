6a4a0a32eafb6499dc67ea5f1588ee081d98b79f
Adrien Thebo <adrien.thebo@gmail.com>
2011-04-26 14:52:21 -0700

    Reworked application and options parsing/handling

--- lib/copycfg/application.rb
+++ lib/copycfg/application.rb
@@ -23,69 +23,76 @@ module Copycfg::Application
       Copycfg.logger.level = options[:verbosity]
       Copycfg.loadconfig options[:configfile]
 
-      if options[:netgroups]
-        copy_netgroup options[:netgroups]
-      end
-
-      if options[:share]
+      case options[:action]
+      when "share"
         Copycfg.shareall
-      end
-      if options[:unshare]
+      when "unshare"
         Copycfg.unshareall
+      when "copy"
+        options[:netgroups].each { | ng | copy_netgroup ng }
+        options[:hosts].each { | h | copy_host h }
       end
     end
 
-    def copy_netgroup(netgroups)
+    def copy_netgroup name
 
-      netgroups.each do | netgroupname |
+      netgroup = Copycfg::Netgroup.new(name, Copycfg.config["ldap"]["connection"], Copycfg.config["ldap"]["base"])
+      hosts = netgroup.gethosts
+      Copycfg.logger.info { "Got #{hosts.size} host(s) from netgroup #{netgroup.name}" }
 
-        netgroup = Copycfg::Netgroup.new(netgroupname, Copycfg.config["ldap"]["connection"], Copycfg.config["ldap"]["base"])
-        hosts = netgroup.gethosts
-        Copycfg.logger.info { "Got #{hosts.size} host(s) from netgroup #{netgroup.name}" }
+      hosts.each { | host | copy_host host }
+    end
 
-        hosts.each do | host |
-          Copycfg.logger.info { "Copying #{host}" }
-          host = Copycfg::Host.new(host, Copycfg.config["basedir"], Copycfg.config["sftp"])
-          host.files_from_yaml Copycfg.config
-          host.copy
-        end
-
-      end
+    def copy_host host
+      Copycfg.logger.info { "Copying #{host}" }
+      host = Copycfg::Host.new(host, Copycfg.config["basedir"], Copycfg.config["sftp"])
+      host.files_from_yaml Copycfg.config
+      host.copy
     end
 
     # Provide default arguments, parse args, and validate them
     def parse(args)
+
       options = {}
       options[:verbosity] = Logger::ERROR
-      options[:netgroups]  = []
+      options[:netgroups] = []
+      options[:hosts] = []
 
       opt_parser = OptionParser.new do |opts|
         opts.banner = "#{$0} [options]"
 
-        opts.on('-c', '--config=val', 'Location of YAML configuration file' ) do |configfile|
-          options[:configfile] = configfile
-        end
+        opts.on('-a', '--action=val', %w{share unshare copy}, 'The action to take',
+                "  share: share all configuration directories",
+                "  unshare: unshare all configuration directories",
+                "  copy: copy configurations off the specified hosts and netgroups"
+        ) do |action|
+          if options[:action]
+            raise ArgumentError, "Error: --action can only be used once"
+          else
+            options[:action] = action
+          end
+       end
 
-        opts.on('-n', '--netgroup=val', 'Netgroup to copycfg-ify' ) do |netgroup|
-          options[:netgroups] << netgroup
-        end
+       opts.on('-n', '--netgroup=val', 'Netgroup to copy configurations from' ) do |netgroup|
+         options[:netgroups] << netgroup
+       end
 
-        opts.on('-s', '--share', 'Share all hosts' ) do
-          options[:share] = true
-        end
+       opts.on('--host=val', 'Netgroup to copy configurations from' ) do |host|
+         options[:hosts] << host
+       end
 
-        opts.on('-u', '--unshare', 'Unshare all hosts' ) do
-          options[:unshare] = true
-        end
+       opts.on('-c', '--config=val', 'Location of YAML configuration file' ) do |configfile|
+         options[:configfile] = configfile
+       end
 
-        opts.on('-v', '--verbose', 'Increase verbosity' ) do
-          options[:verbosity] -= 1
-        end
+       opts.on('-v', '--verbose', 'Increase verbosity' ) do
+         options[:verbosity] -= 1
+       end
 
-        opts.on('-h', '--help', 'Display this help') do
-          puts opts
-          exit
-        end
+       opts.on_tail('-h', '--help', 'Display this help') do
+         puts opts
+         exit
+       end
 
       end
 
@@ -94,12 +101,13 @@ module Copycfg::Application
         opt_parser.parse! args
       rescue
         $stderr.puts $!
+        $stderr.puts opt_parser
         exit 1
       end
 
       # Ensure that we have at least one netgroup and we're not just resharing
-      if options[:netgroups].length <= 0 and not options[:share] and not options[:unshare]
-        $stderr.puts "Error: nothing to do, no operation specified"
+      unless options[:action]
+        $stderr.puts "Error: no operation specified"
         puts opt_parser
         exit 1
       end
