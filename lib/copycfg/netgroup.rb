44450a57242d6e8742de40f28d09aee11bccce77
Adrien Thebo <adrien.thebo@gmail.com>
2011-04-25 19:58:02 -0700

    Reworked ldap netgroup querying

--- lib/copycfg/netgroup.rb
+++ lib/copycfg/netgroup.rb
@@ -1,15 +1,16 @@
 # Copycfg::Netgroup
 # Computer Action Team
 # Maseeh College of Engineering and Computer Science
-# 
+#
 # Queries LDAP to retrieve hosts in a netgroup.
 
 require "rubygems"
 require "net/ldap"
 
-class Copycfg::Netgroup 
+class Copycfg::Netgroup
 
   attr_reader :hosts
+  attr_reader :name
 
   def initialize netgroupname, auth
     @name = netgroupname
@@ -25,24 +26,26 @@ class Copycfg::Netgroup
 
     attrs = %w[ nisNetgroupTriple memberNisNetgroup ]
 
-    @ldap.search( :base => Copycfg::Config["ldap"]["base"],
-                  :filter => filter, 
-                  :attributes => attrs) do | entry |
+    @ldap.open do
+      @ldap.search( :base => Copycfg::Config["ldap"]["base"],
+                    :filter => filter,
+                    :attributes => attrs) do | entry |
 
-      entry["nisNetgroupTriple"].each do | triple |
-        if triple =~ /\(([^,]+),-,\)/
-          @hosts << $1
-        else
-          Copycfg.logger.info { "Got mangled nisNetgroupTriple: #{triple}"}
+        entry["nisNetgroupTriple"].each do | triple |
+          if triple =~ /\(([^,]+),-,\)/
+            @hosts << $1
+          else
+            Copycfg.logger.info { "Got mangled nisNetgroupTriple: #{triple}"}
+          end
         end
-      end
 
-      entry["memberNisNetgroup"].each do | member |
-        # It's objects all the way down!
-        # AKA create and query all member netgroups, and add their hosts.
-        @hosts += Copycfg::Netgroup.new(member, @auth).gethosts
-      end
+        entry["memberNisNetgroup"].each do | member |
+          # It's objects all the way down!
+          # AKA create and query all member netgroups, and add their hosts.
+          @hosts += Copycfg::Netgroup.new(member, @auth).gethosts
+        end
 
+      end
     end
 
     @hosts.sort!
